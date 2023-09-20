733572b6817c22163f4e195e138c2dce29d66905
Dirk Rademann <dirk.rademann@icloud.com>
2023-09-20 21:06:01 +0200

    initial commit

---
+++ Haora/Haora/HaoraApp.swift
@@ -0,0 +1,25 @@
+import SwiftUI
+import SwiftData
+
+@main
+struct HaoraApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
