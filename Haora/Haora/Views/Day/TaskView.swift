cccc8545903ffe7d2f1b4a4b17ff3eebd1638d2e
Dirk Rademann <dirk.rademann@icloud.com>
2023-10-07 12:03:21 +0200

    feature(#4): first simple design for a task view

---
+++ Haora/Haora/Views/Day/TaskView.swift
@@ -0,0 +1,27 @@
+import SwiftUI
+
+struct TaskView: View {
+    var body: some View {
+        ZStack {
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Working on project Haora")
+                    Text("#Haora")
+                        .foregroundStyle(.gray)
+                }
+                Spacer()
+            }
+            HStack {
+                Spacer()
+                VStack(alignment: .trailing) {
+                    Text("9:15 - 12:00")
+                    Text("2 h 45 m" )
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    TaskView()
+}
