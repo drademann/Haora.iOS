5f4c5a72b32ba6bc2257fd0395cf3d9e71b04b24
Dirk Rademann <dirk.rademann@icloud.com>
2023-10-08 17:15:56 +0200

    feature(#4): basic layout for day view

--- Haora/Haora/Views/Day/DayView.swift
+++ Haora/Haora/Views/Day/DayView.swift
@@ -4,7 +4,28 @@ struct DayView: View {
     
     var body: some View {
         NavigationView {
-            
+            VStack {
+                VStack {
+                    Text(Date(), style: .date)
+                    Text("Sunday")
+                }
+                Divider()
+                List {
+                    ForEach(0..<4) { _ in
+                        NavigationLink {} label: { TaskView() }
+                    }
+                }
+                .listStyle(.plain)
+                Divider()
+                HStack {
+                    Text("Total")
+                    Spacer()
+                    Text("11 h")
+                }
+                .padding(.top, 5)
+                .padding(.leading, 20)
+                .padding(.trailing, 40)
+            }
         }
         .tabItem {
             Label("Day", systemImage: "1.circle")
