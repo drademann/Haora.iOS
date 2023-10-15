8bc057d1471c2d6b8348c155570425a6c6ce5760
Dirk Rademann <dirk.rademann@icloud.com>
2023-10-15 12:35:21 +0200

    feature(#2): adds tags to Task's data model

--- Haora/Haora/Model/Task.swift
+++ Haora/Haora/Model/Task.swift
@@ -6,10 +6,12 @@ final class Task {
     var start: Date
     var text: String
     var isPause: Bool
+    var tags: [String]
     
-    init(start: Date, text: String, isPause: Bool) {
+    init(start: Date = Date(), text: String, isPause: Bool = false, tags: [String] = []) {
         self.start = start
         self.text = text
         self.isPause = isPause
+        self.tags = tags
     }
 }
