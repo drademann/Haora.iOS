3f98009eb927fc344800722fd18da16c8aa6ed66
Dirk Rademann <dirk.rademann@icloud.com>
2023-11-02 18:24:06 +0100

    feature(#14): now displays weekday

--- Haora/Haora/Views/Day/DayView.swift
+++ Haora/Haora/Views/Day/DayView.swift
@@ -16,7 +16,7 @@ struct DayView: View {
             VStack {
                 Text(date, style: .date)
                     .font(.largeTitle)
-                Text("Sunday")
+                Text(weekday(of: date))
                     .font(.title2)
                 
                 if day.tasks.isEmpty {
@@ -35,6 +35,16 @@ struct DayView: View {
             Label("Day", systemImage: "1.circle")
         }
     }
+}
+
+extension DayView {
+    
+    private func weekday(of date: Date) -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "EEEE"
+        return formatter.string(from: date)
+    }
+    
 }
 
 extension DayView {
