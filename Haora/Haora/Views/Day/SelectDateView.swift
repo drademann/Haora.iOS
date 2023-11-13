1dae5f87297931f456540dddbc401163da2d2222
Dirk Rademann <dirk.rademann@icloud.com>
2023-11-13 21:54:55 +0100

    now allows to switch days

--- Haora/Haora/Views/Day/SelectDateView.swift
+++ Haora/Haora/Views/Day/SelectDateView.swift
@@ -7,24 +7,36 @@ struct SelectDateView: View {
     var body: some View {
         ZStack {
             HStack {
-                Button(action: {}) {
+                Button(action: { switchDay(day: -1) }) {
                     Label("previous day", systemImage: "chevron.left")
                 }
                 Spacer()
-                Button(action: {}) {
+                Button(action: { switchDay(day: +1) }) {
                     Label("next day", systemImage: "chevron.right")
                         .labelStyle(TrailingImageLabelStyle())
                 }
             }
             HStack {
                 Spacer()
-                Button(action: {}) {
+                Button(action: today) {
                     Text("today")
                         .tint(.secondary)
                 }
                 Spacer()
             }
         }
+    }
+    
+    private func switchDay(day: Int) {
+        let components = DateComponents(day: day)
+        guard let changedDay = Calendar.current.date(byAdding: components, to: self.date) else {
+            fatalError("unable to switch from \(date) by \(day) day")
+        }
+        self.date = changedDay.withoutTime()
+    }
+    
+    private func today() {
+        self.date = Date().withoutTime()
     }
 }
 
