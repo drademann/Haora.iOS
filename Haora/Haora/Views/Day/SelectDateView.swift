1204a09808f2f1f1c7e3c91aa61ade6ce4970b9a
Dirk Rademann <dirk.rademann@icloud.com>
2023-11-18 10:49:16 +0100

    beautification on select day view

--- Haora/Haora/Views/Day/SelectDateView.swift
+++ Haora/Haora/Views/Day/SelectDateView.swift
@@ -7,11 +7,11 @@ struct SelectDateView: View {
     var body: some View {
         ZStack {
             HStack {
-                Button(action: { switchDay(day: -1) }) {
+                Button(action: { switchDay(to: .previous) }) {
                     Label("previous day", systemImage: "chevron.left")
                 }
                 Spacer()
-                Button(action: { switchDay(day: +1) }) {
+                Button(action: { switchDay(to: .next) }) {
                     Label("next day", systemImage: "chevron.right")
                         .labelStyle(TrailingImageLabelStyle())
                 }
@@ -27,7 +27,17 @@ struct SelectDateView: View {
         }
     }
     
-    private func switchDay(day: Int) {
+    enum Direction {
+        case next
+        case previous
+    }
+    
+    private func today() {
+        self.date = Date().withoutTime()
+    }
+    
+    private func switchDay(to direction: Direction) {
+        let day = daysToSwitch(into: direction)
         let components = DateComponents(day: day)
         guard let changedDay = Calendar.current.date(byAdding: components, to: self.date) else {
             fatalError("unable to switch from \(date) by \(day) day")
@@ -35,8 +45,11 @@ struct SelectDateView: View {
         self.date = changedDay.withoutTime()
     }
     
-    private func today() {
-        self.date = Date().withoutTime()
+    private func daysToSwitch(into direction: Direction) -> Int {
+        return switch (direction) {
+            case .next: 1
+            case .previous: -1
+        }
     }
 }
 
