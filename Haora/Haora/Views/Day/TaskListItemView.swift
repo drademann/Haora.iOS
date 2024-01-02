9c4c0b51e918a13c861a41dc67dbd9e51cc70798
Dirk Rademann <dirk.rademann@icloud.com>
2024-01-02 17:26:51 +0100

    fixes display of breaks (#22)

--- Haora/Haora/Views/Day/TaskListItemView.swift
+++ Haora/Haora/Views/Day/TaskListItemView.swift
@@ -10,7 +10,7 @@ struct TaskListItemView: View {
         ZStack {
             HStack {
                 VStack(alignment: .leading) {
-                    Text(task.text)
+                    Text(taskText)
                         .foregroundStyle(taskTextForegroundStyle)
                         .font(.headline)
                     Text(asString(task.tags))
@@ -23,8 +23,10 @@ struct TaskListItemView: View {
                 VStack(alignment: .trailing) {
                     Text(start)
                         .font(.headline)
+                        .foregroundStyle(taskTextForegroundStyle)
                     Text(duration)
                         .font(.headline)
+                        .foregroundStyle(taskTextForegroundStyle)
                 }
             }
         }
@@ -33,17 +35,25 @@ struct TaskListItemView: View {
 
 extension TaskListItemView {
     
-    private var taskTextForegroundStyle: Color { get {
-        return if task.isBreak { .secondary } else { .primary }
-    }}
+    private var taskText: String {
+        if task.isBreak && task.text.isEmpty {
+            "Break"
+        } else {
+            task.text
+        }
+    }
     
-    private var start: String { get {
-        return task.start.asTimeString()
-    }}
+    private var taskTextForegroundStyle: Color {
+        if task.isBreak { .secondary } else { .primary }
+    }
     
-    private var duration: String { get {
-        return task.duration(to: task.successor(), using: time).asString()
-    }}
+    private var start: String {
+        task.start.asTimeString()
+    }
+    
+    private var duration: String {
+        task.duration(to: task.successor(), using: time).asString()
+    }
 }
 
 #Preview {
